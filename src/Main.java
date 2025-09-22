dcc7e408a978ea912da1f50ee7dafcf41d0c5eb1
Alexander Chen <alexanderc416@nycstudents.net>
2025-09-22 14:38:09 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your total bill amount: ");
+        double totalBill = input.nextDouble();
+
+        System.out.println("What is the Tip Percent (as a int): ");
+        int tipPercent = input.nextInt();
+
+    }
+}
