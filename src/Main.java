f35d4efe4231e5699b720a889f8bc66b3fcfe8ea
AlexanderChen <hello@gmail.com>
2025-09-22 14:43:36 -0400

    Added var's

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         System.out.print("Enter your total bill amount: ");
         double totalBill = input.nextDouble();
 
-        System.out.println("What is the Tip Percent (as a int): ");
+        System.out.print("What is the Tip Percent (as a int): ");
         int tipPercent = input.nextInt();
 
+        System.out.print("What is the number of People: ");
+        int numberOfPeople = input.nextInt();
+
+        System.out.println("bill: " + totalBill + " tip: " + tipPercent + " ppl: " + numberOfPeople);
     }
 }
