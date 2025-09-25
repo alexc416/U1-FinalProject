7b24074ec9c6b11d1645aea7872aad25f4581531
AlexanderChen <hello@gmail.com>
2025-09-25 14:41:14 -0400

    Trying to format

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,17 @@ public class Main {
         System.out.print("What is the number of People: ");
         int numberOfPeople = input.nextInt();
 
+        double totalTip = (totalBill) * (tipPercent / 100.0);
+        totalBill = totalBill + totalTip;
+        double tipPerPerson = totalTip / numberOfPeople;
+        double totalPerPerson = totalBill / numberOfPeople;
+        System.out.println(totalTip);
+        totalTip = ((int) ((totalTip*100.0)+0.5) / 100);
+        /*totalBill = (((int) (totalBill * 100) + 0.5) / 100);
+        tipPerPerson = (((int) (tipPerPerson * 100) + 0.5) / 100);
+        totalPerPerson = (((int) (totalPerPerson * 100) + 0.5) / 100);*/
+
+        System.out.println(" total tip: " + totalTip + " total bill cost: " + totalBill + " tip per person: " + tipPerPerson + " total bill per person: " + totalPerPerson);
         System.out.println("bill: " + totalBill + " tip: " + tipPercent + " ppl: " + numberOfPeople);
     }
 }
